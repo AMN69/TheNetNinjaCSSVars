059f7a7c45a81c1e7b005ba8a96e62b627e38d31
AMN69 <martinez.andreu@gmail.com>
2021-06-11 11:14:34 +0200

    20210611 playing with CSS variables and understanding them

--- swatches.js
+++ swatches.js
@@ -1,5 +1,10 @@
-var swatches = document.querySelectorAll('.swatches span');
-var root = document.querySelector(':root');
+// AMN - Unless SASS, CSS vars can be modified through Javascript
+
+var swatches = document.querySelectorAll('.swatches span'); // AMN - we grab all four span
+var root = document.querySelector(':root'); // AMN - we grab the html tag
+
+// console.log("Swatches: ", swatches);
+// console.log("root: ", root);
 
 swatches.forEach((swatch) => {
     swatch.addEventListener('click', (e) => {
